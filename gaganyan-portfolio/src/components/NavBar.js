17a83c3655b732f009d6c97582426467d2f0c694
Chetan <cheatnchinchulkar635@gmail.com>
2023-08-26 00:29:44 +0530

    Added nav Bar and its components( only view/ no functions )

--- gaganyan-portfolio/src/components/NavBar.js
+++ gaganyan-portfolio/src/components/NavBar.js
@@ -3,7 +3,19 @@ import '../styles/NavBar.css'
 export default function navBar() {
   return (
     <div className="Navbody">
-      this is nav bar
+      <div className="profileName">
+        Chetan Chinchulkar.
+      </div>
+      <div className="profileNavigation">
+        <ul className='profileNavigation_Items'>
+          <li>HOME</li>
+          <li>SKILLS</li>
+          <li>WORKS</li>
+          <li>RESUME</li>
+          <li>CONTACTS</li>
+        </ul>
+        <button className='profileNavigation_HireMe'>HIRE ME</button>
+      </div>
     </div>
   )
 }
